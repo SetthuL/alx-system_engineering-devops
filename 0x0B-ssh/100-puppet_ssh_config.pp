f302b70cdb8b418bedabd2e8ba2818f47a1a26d0
SetthuL <langesiphosethu@gmail.com>
2024-03-24 12:56:05 -0700

    100-puppet_ssh_config.pp updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # using Puppet to make changes to our configuration file
-file { 'etc/ssh/ssh_config':
-	ensure => Present,
+file { 'etc/ssh/ssh_cofig':
+	ensure => present,
 content =>"
 #SSH Client configuration
 host*
